f529176d51b466516ab9d66cb9b71f5d3d199170
Nehaa992 <nehaa992@gmail.com>
2015-11-25 17:55:26 +0530

    second update
    for tv final page yet to be completed, by adding animation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var mm=0;
 var ss=0;
 var time=1000;
 var timeCurrent = 0;
-var timeComplete = 60;
+var timeComplete = 3600;
 
 var incrementTimer = function() {
     width = $("#fill").width();
@@ -65,3 +65,35 @@ var toggleTimer = function(){
     }
 }
 
+
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 13) {
+    toggleFullScreen();
+  }
+}, false);
+
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
--- timer_replay.js
+++ timer_replay.js
@@ -1,6 +1,4 @@
 var time=1000;
-var mm=0;
-var ss=0;
 var timeCurrent = 0;
 var timeComplete = 60;
 function toggleTimer() {
@@ -10,29 +8,7 @@ function alertFunc() {
    self.location="timer-on.html";
     }
 var incrementTimer = function() {    
-    ss=ss+1;
-    if(ss<10)
-    {
-        document.getElementById("sec").innerHTML ="0"+ ss;
-    }
-    else
-    {
-        document.getElementById("sec").innerHTML = ss;
-    }
-    if(ss==60) 
-    {
-        ss= 0;
-        document.getElementById("sec").innerHTML = ss;
-        mm=mm+1;
-        if(mm<10)
-        {
-            var x = document.getElementById("min").innerHTML ="0"+ mm;
-        }
-        else
-        {
-            var x = document.getElementById("min").innerHTML = mm;
-        }
-    }
+    
     timeCurrent += 1;  
 }
 var timer;
@@ -41,3 +17,34 @@ var startTimer = function() {
 }
 toggleTimer();
 startTimer();
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 13) {
+    toggleFullScreen();
+  }
+}, false);
+
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
